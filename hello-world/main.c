7e7c3584d8b3755315d584d22f151e797f5dfbc8
Alistair Smith <alistairsmith01@gmail.com>
2021-08-14 16:58:22 +0100

    c

--- hello-world/main.c
+++ hello-world/main.c
@@ -4,12 +4,9 @@
 #include "pico/stdlib.h"
 
 const uint16_t DELAYS[] = {100, 250, 300, 350, 400, 500, 750, 300};
+const int SIZE = sizeof(DELAYS) / sizeof(DELAYS[0]);
 
-uint16_t get_delay() {
-	const int x = sizeof(DELAYS[0]);
-	const int SIZE = sizeof(DELAYS) / sizeof(DELAYS[0]);
-	return DELAYS[rand() % SIZE];
-}
+uint16_t get_delay() { return DELAYS[rand() % SIZE]; }
 
 int main() {
 	stdio_init_all();
