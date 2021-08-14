c3923fd1d98e6d4b8e0a2c2df6b7f7047e814249
Alistair Smith <alistairsmith01@gmail.com>
2021-08-14 17:53:49 +0100

    c

--- hello-world/delay.c
+++ hello-world/delay.c
@@ -3,9 +3,10 @@
 #include <time.h>
 
 #include "pico/stdlib.h"
+#include "util.h"
 
 const uint16_t DELAYS[] = {100, 250, 300, 350, 400, 500, 750, 300};
-const int SIZE = sizeof(DELAYS) / sizeof(DELAYS[0]);
+const int SIZE = ARR_LENGTH(DELAYS);
 
 uint16_t get_delay() {
 	return DELAYS[rand() % SIZE];
---
+++ hello-world/util.h
@@ -0,0 +1,2 @@
+#pragma once
+#define ARR_LENGTH(arr) sizeof(arr) / sizeof(arr[0])
