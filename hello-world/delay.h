8a82453fed47c2cd71da456f82ec6801715c30e0
Alistair Smith <alistairsmith01@gmail.com>
2021-08-14 17:12:27 +0100

    epic working methd

---
+++ hello-world/delay.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <time.h>
+#include "pico/stdlib.h"
+
+int16_t get_delay();
--- hello-world/main.c
+++ hello-world/main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+
 #include "pico/stdlib.h"
-
-const uint16_t DELAYS[] = {100, 250, 300, 350, 400, 500, 750, 300};
-const int SIZE = sizeof(DELAYS) / sizeof(DELAYS[0]);
-
-uint16_t get_delay() { return DELAYS[rand() % SIZE]; }
+#include "delay.h"
 
 int main() {
 	stdio_init_all();
