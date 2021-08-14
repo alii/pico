55263de7a5aeffe917a4323b2eb9605d8e997e24
Alistair Smith <alistairsmith01@gmail.com>
2021-08-14 17:19:52 +0100

    move docstring

--- hello-world/delay.c
+++ hello-world/delay.c
@@ -5,9 +5,6 @@
 const uint16_t DELAYS[] = {100, 250, 300, 350, 400, 500, 750, 300};
 const int SIZE = sizeof(DELAYS) / sizeof(DELAYS[0]);
 
-/**
- * Gets a random delay to be used as an interval between LED flashes
- */
 uint16_t get_delay() {
   return DELAYS[rand() % SIZE];
 }
--- hello-world/delay.h
+++ hello-world/delay.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include <time.h>
 #include "pico/stdlib.h"
 
+/**
+ * Gets a random delay to be used as an interval between LED flashes
+ */
 int16_t get_delay();
